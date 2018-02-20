188e581b2df6e91d8519a3adecb10d2abf3c3dfe
Matthew Mogavero <matt.moga@gmail.com>
2018-02-19 20:29:50 -0500

    refactored UserInformation into a functional stateless component

--- src/UserInformation.js
+++ src/UserInformation.js
@@ -1,30 +1,34 @@
-import React, { Component } from 'react';
-import './UserInformation.css';
+import React from "react";
+import "./UserInformation.css";
 
-class UserInformation extends Component {
-    render() {
-        const { info } = this.props;
-        if (!info) {
-            return <h1 className='UserInformation--header vertical-center'>Please Select a User</h1>
-        }
-        return (
-            <div className="UserInformation--container">
-                <h1 className="UserInformation--header">
-                    {info.name}
-                    <small>({info.login})</small>
-                </h1>
-                <div className="UserInformation--avatar-container">
-                    <img src={info.avatar_url}
-                        alt={info.login}
-                        className="UserInformation--avatar" />
-                </div>
-                <div className="UserInformation--metrics">
-                    <p>Followers: {info.followers}</p> 
-                    <p>Gists: {info.public_gists}</p>
-                </div>
-            </div>
-        )
-    }
-}
+const UserInformation = props => {
+  const { info } = props;
+  if (!info) {
+    return (
+      <h1 className="UserInformation--header vertical-center">
+        Please Select a User
+      </h1>
+    );
+  }
+  return (
+    <div className="UserInformation--container">
+      <h1 className="UserInformation--header">
+        {info.name}
+        <small>({info.login})</small>
+      </h1>
+      <div className="UserInformation--avatar-container">
+        <img
+          src={info.avatar_url}
+          alt={info.login}
+          className="UserInformation--avatar"
+        />
+      </div>
+      <div className="UserInformation--metrics">
+        <p>Followers: {info.followers}</p>
+        <p>Gists: {info.public_gists}</p>
+      </div>
+    </div>
+  );
+};
 
 export default UserInformation;
